d50112c3635b66e09c415ab55efed1f992fa07d3
Guilherme Barros JR <dida0982@gmail.com>
2024-11-05 10:38:46 -0300

    feat(app,js): add import de dois arquivos novos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Banner from "./componentes/Banner/Banner";
+import Banner from "./components/Banner/Banner";
+import TextField from "./components/TextField";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bem vindo ao React!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField/>
     </div>
   );
 }
