223edc07f24257e6d684d24da0a6c790cccb11b1
Guilherme Barros JR <dida0982@gmail.com>
2024-11-05 10:47:19 -0300

    Create Banner.js
    Atualizações no Estilo dos Componentes de Texto:
    
    - Adicionada margem vertical de 24px para todos os elementos com a classe .compo-texto, melhorando o espaçamento vertical sem alterar as margens laterais.
    - Estilização dos labels dentro de .campo-texto para exibir como blocos, com uma margem inferior de 8px e tamanho de fonte aumentado para 24px, tornando-os mais legíveis e bem espaçados dos elementos abaixo.
    - Configurado o background dos inputs dentro de .campo-texto para branco (#fff) e ajustado a largura para ocupar 100% do contêiner, garantindo uma apresentação consistente e limpa. A sombra caixa está definida mas completamente transparente, permitindo ajustes futuros sem impacto visual atual.

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+function Banner() {
+  //JSX
+
+  return (
+    <header className="banner">
+      <img src="/imgs/banner.png" alt="img banner" />;
+    </header>
+  );
+}
+
+export default Banner;
