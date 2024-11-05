f75100d96624ceb2984184218e9ba060e0841987
Guilherme Barros JR <dida0982@gmail.com>
2024-11-05 10:48:24 -0300

    Create index.js
    Implementação do Componente TextField:
    
    - O componente `TextField` é um componente React funcional.
    - Ele importa o arquivo de estilos `TextField.css` para aplicar formatação específica.
    - Dentro do JSX, o componente retorna um contêiner `div` com a classe `campo-texto`.
    - Dentro deste `div`, possui um elemento `label` contendo o texto "Nome", que serve como um rótulo para o campo de entrada.
    - Abaixo do `label`, há um elemento `input` com um placeholder que instrui o usuário a "Digite o seu nome". Este campo é onde o usuário pode inserir seu nome.
    - O componente é exportado como `default`, tornando-o reutilizável em outras partes do aplicativo onde um campo de entrada de texto é necessário.
    
    Este componente é ideal para formulários onde o usuário precisa inserir seu nome, fornecendo um estilo consistente e funcionalidade clara.

---
+++ src/components/TextField/index.js
@@ -0,0 +1,12 @@
+import './TextField.css'
+
+const TextField = () => {
+    return (
+        <div className='campo-texto'>
+            <label>Nome</label>   
+            <input placeholder='Digite o seu nome'/>
+        </div>
+    )
+}
+    
+export default TextField
