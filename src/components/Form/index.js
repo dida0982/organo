c2bb1aff1fbb2e090aae43128d3029ae70064552
Guilherme Barros JR <dida0982@gmail.com>
2024-11-06 12:10:40 -0300

    Create Form component with input fields, dropdown, and submit button
    Adiciona o componente Form, que inclui campos de entrada (TextField) para nome, cargo e imagem, um menu suspenso (DropDownList) para seleção de equipes e um botão (Button) para criar um cartão. O formulário permite ao usuário preencher e submeter informações básicas.

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,31 @@
+import Button from "../Button";
+import DropDownList from "../DropDownList";
 import TextField from "../TextField";
 import "./Form.css";
 
-
 const Form = () => {
+  const teams = [
+    "Programmer",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Innovation and management"
+  ];
+
   return (
     <section className="form">
       <from>
         <h2>Fill in the data</h2>
-        <TextField label="Name" placeholder="Enter your name"/>
-        <TextField label="Position" placeholder="Enter your position"/>
-        <TextField label="Image" placeholder="Enter the address of your image"/>
+        <TextField label="Name" placeholder="Enter your name" />
+        <TextField label="Position" placeholder="Enter your position" />
+        <TextField
+          label="Image"
+          placeholder="Enter the address of your image"
+        />
+        <DropDownList label="Teams" items={teams} />
+        <Button>Create card</Button>
       </from>
     </section>
   );
