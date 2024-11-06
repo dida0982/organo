1c08380c966d527e9bda0ddd3176bf8e07793171
Guilherme Barros JR <dida0982@gmail.com>
2024-11-06 13:21:32 -0300

    teste git

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -2,10 +2,23 @@ import "./TextField.css";
 
 const TextField = (props) => {
   const placeholderModification = `${props.placeholder}...`;
+
+  let value = "";
+
+  const whenTyped = (event) => {
+    value = event.target.value;
+    console.log(value);
+  };
+
   return (
     <div className="field-text">
       <label>{props.label}</label>
-      <input required={props.mandatory} placeholder={placeholderModification} />
+      <input
+        value={value}
+        onChange={whenTyped}
+        required={props.mandatory}
+        placeholder={placeholderModification}
+      />
     </div>
   );
 };
