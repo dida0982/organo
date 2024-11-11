7eb4fbf27a2d8bc7b3425c36ce47c18bb4384730
Guilherme Barros JR <dida0982@gmail.com>
2024-11-11 11:51:18 -0300

    Update App.js
    Vamos entender cada parte desta linha de código:
    
    javascript
    Copiar código
    import { useState } from "react";
    1. import
    A palavra-chave import é usada no JavaScript (e no React) para carregar uma funcionalidade ou módulo de outro arquivo ou biblioteca. Neste caso, ela está trazendo uma função específica (useState) de uma biblioteca chamada "react".
    
    2. { useState }
    As chaves { } indicam que estamos usando desestruturação para importar apenas uma parte específica do módulo. Em JavaScript, quando usamos { } após o import, estamos dizendo que queremos apenas certos elementos do módulo, não o módulo inteiro.
    
    useState é um hook do React, uma função especial que permite usar o estado dentro de componentes funcionais. Em outras palavras, ele permite que você crie e gerencie variáveis de estado em componentes do React.
    
    3. from
    A palavra-chave from é usada para indicar de onde estamos importando o módulo ou a funcionalidade. Neste caso, ela está dizendo de qual biblioteca estamos trazendo o useState.
    
    4. "react"
    Aqui está o nome da biblioteca de onde estamos importando o useState. "react" refere-se ao pacote principal do React, que fornece funcionalidades fundamentais para criar interfaces de usuário. Ao usar o React, ele é instalado como uma dependência do projeto, e podemos acessá-lo diretamente pelo nome "react".
    
    Resumo
    Esta linha de código está importando o hook useState do React, para que você possa usá-lo no seu componente para gerenciar o estado.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Banner from "./components/Banner/Banner";
 import Form from "./components/Form";
 
 function App() {
+  const [collaborators, setCollaborator] = useState([]);
+
+  const toTheNewRegisteredCollaborator = (collaborator) => {
+    console.log(collaborator);
+    setCollaborator([...collaborators, collaborator])
+  };
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form
+        toTheRegisteredCollaborator={(collaborator) =>
+          toTheNewRegisteredCollaborator(collaborator)
+        }
+      />
     </div>
   );
 }
